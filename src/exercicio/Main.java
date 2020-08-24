5a8812a7b7c82b346d6988fb1e491ceafb31f4bd
Vitor Santos <sebastiao.vitor@souunit.com.br>
2020-08-23 21:22:03 -0300

    Primeiro Exercicio

---
+++ src/exercicio/Main.java
@@ -0,0 +1,14 @@
+
+package exercicio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal1 = new Pomba();
+        animal1.imprimirCaracterisca();
+        
+        Animal animal2 = new Cachorro();
+        animal2.imprimirCaracterisca();
+    }
+    
+}
